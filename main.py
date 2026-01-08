70468305587c145fcdb51c5f4494b1a05398b24f
hpeilun <lun0412@gmail.com>
2026-01-08 08:53:48 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from kivy.app import App
+from kivy.uix.label import Label
+
+class Strategy2560(App):
+    def build(self):
+        return Label(text="2560 Strategy APK Running")
+
+Strategy2560().run()
